438d0e9d7365c257d7774e703f788efe2b350113
ashtam543 <115128341+ashtam543@users.noreply.github.com>
2024-01-04 01:34:44 +0530

    Initial commit
    Hi.. this is initial commit for jenkins

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public  class Main{
         System.out.println("This is a java program");
         System.out.println("output from java program");
         System.out.println("Hello");
+        System.out.println("Hello github");
     }
 }
